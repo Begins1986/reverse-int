83679985847f0f20a8f9ad449e7b9e6a3d36c177
Begin Sergey <begins.1986@mail.ru>
2020-03-06 22:51:05 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        n = n
+    } else {
+        n = String(n * (-1));
+    }
+    let str = String(n);
+    let splitStr = str.split('');
+    let reverseArray = splitStr.reverse();
+    let joinStr = reverseArray.join('');
+    let num = Number(joinStr);
+    console.log(num);
+    return num;
 }
